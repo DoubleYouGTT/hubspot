31e7f51a4adeb2c757357050d4a147b76691bb35
Steph Locke <steph@itsalocke.com>
2018-12-22 14:16:23 +0000

    Added deal_associations()

---
+++ R/deal_associations.R
@@ -0,0 +1,31 @@
+#' Extract a table of associations from a deals list
+#'
+#' @inheritParams get_deal_properties
+#' @param deals A deals list object, typically produced by running `get_deals`
+#'
+#' @return A tibble with associated entities
+#' @export
+#'
+#' @examples
+#' deal_associations()
+deal_associations <- function(apikey = "demo",
+                              deals = get_deals(apikey,
+                                max_iter = 10,
+                                max_properties = 1,
+                                associations = "true"
+                              )) {
+  # Variable fudge for check()
+  dealId <- Ids <- `.` <- NULL
+
+  deals %>%
+    map("associations") %>%
+    data_frame(Ids = .) %>%
+    unnest(.id = "dealId") %>%
+    mutate(dealId = as.integer(dealId)) %>%
+    mutate(type = rep(
+      c("contacts", "companies", "deals"),
+      n() / 3
+    )) %>%
+    unnest(Ids) %>%
+    unnest(Ids)
+}
---
+++ tests/testthat/test-deal_associations.R
@@ -0,0 +1,8 @@
+context("test-deal_associations")
+
+test_that("deal_associations works", {
+  res <- deal_associations()
+  expect_is(res, "tbl_df")
+  expect_equal(ncol(res), 3)
+  expect_equal(colnames(res), c("dealId", "type", "Ids"))
+})
