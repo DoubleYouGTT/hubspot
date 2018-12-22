4cdd96c1c78e60611ca9c7bd487d70ecd43722a3
Steph Locke <steph@itsalocke.com>
2018-12-22 15:28:25 +0000

    Added deal_properties

---
+++ R/deal_properties.R
@@ -0,0 +1,19 @@
+#' Extract a table of deal IDs and simple property values
+#'
+#' @inheritParams deal_associations
+#'
+#' @return A tibble with associated entities
+#' @export
+#'
+#' @examples
+#' deals = deal_properties()
+deal_properties = function(deals = get_deals(max_iter = 1)){
+  deals %>%
+    map("properties") %>%
+    modify_depth(2,~.$value) %>%
+    map_df(as_data_frame, .id = "dealId") %>%
+    mutate_if(~sum(is.na(.))==sum(is.na(as.numeric(.))), as.numeric)  ->
+    result
+
+  return(result)
+}
