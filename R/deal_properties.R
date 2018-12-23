52e9353f06e0cce98bbb39f345ac97f2d57192f7
Steph Locke <steph@itsalocke.com>
2018-12-23 13:18:06 +0000

    lint

--- R/deal_properties.R
+++ R/deal_properties.R
@@ -12,7 +12,7 @@ deal_properties <- function(deals = get_deals(max_iter = 1)) {
     map("properties") %>%
     modify_depth(2, ~ .$value) %>%
     map_df(as_data_frame, .id = "dealId") %>%
-    mutate_if(~ sum(is.na(.)) == sum(is.na(as.numeric(.))), as.numeric)  %>%
+    mutate_if(~ sum(is.na(.)) == sum(is.na(as.numeric(.))), as.numeric) %>%
     epoch_converter() ->
   result
 
--- R/utils-epoch_converter.R
+++ R/utils-epoch_converter.R
@@ -3,12 +3,17 @@
 #' @param dt A tbl
 #' @return A tbl
 #'
-epoch_converter = function(dt){
-  `.`<-NULL
+epoch_converter <- function(dt) {
+  `.` <- NULL
   dt %>%
-     mutate_at(vars(contains("timestamp"),
-                    contains("lastmodified")),
-               ~ ifelse(is.numeric(.),
-                      as.POSIXct(./1000, origin = "1970-01-01"),
-                      .))
+    mutate_at(
+      vars(
+        contains("timestamp"),
+        contains("lastmodified")
+      ),
+      ~ ifelse(is.numeric(.),
+        as.POSIXct(. / 1000, origin = "1970-01-01"),
+        .
+      )
+    )
 }
