16ed96f637177ab0c1bbac5a65f6fe19a6882ec1
Steph Locke <steph@itsalocke.com>
2018-12-23 15:26:47 +0000

    add deal_properties_history

---
+++ R/deal_properties_history.R
@@ -0,0 +1,23 @@
+#' Retrieve the changes of all properties for a given set of deals
+#'
+#' @inheritParams deal_associations
+#'
+#' @return A tbl of all field changes over time
+#' @export
+#'
+#' @examples
+#' hist <- deal_properties_history()
+deal_properties_history = function(deals = get_deals(
+                                      property_history = "true",
+                                      associations = "false",
+                                      max_iter = 1
+)){
+  deals %>%
+    map("properties") %>%
+    map(flatten) %>%
+    map("versions") %>%
+    flatten() %>%
+    modify_depth(2, ~ifelse(length(.)==0,list(NA_integer_),.)) %>%
+    map_df(as_data_frame, .id = "dealId") %>%
+    epoch_converter()
+}
