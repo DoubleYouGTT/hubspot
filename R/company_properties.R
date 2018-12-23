e9877331dc9123e58d488dfbafac22fd496b4e76
Steph Locke <steph@itsalocke.com>
2018-12-23 12:41:58 +0000

    company_properties

---
+++ R/company_properties.R
@@ -0,0 +1,19 @@
+#' Extract a table of company IDs and simple property values
+#'
+#' @param companies A companies return from `get_companies()`
+#'
+#' @return A tibble with associated entities
+#' @export
+#'
+#' @examples
+#' companies <- company_properties()
+company_properties <- function(companies = get_companies(max_iter = 1)) {
+  companies %>%
+    map("properties") %>%
+    modify_depth(2, ~ .$value) %>%
+    map_df(as_data_frame, .id = "companyId") %>%
+    mutate_if(~ sum(is.na(.)) == sum(is.na(as.numeric(.))), as.numeric) ->
+  result
+
+  return(result)
+}
---
+++ tests/testthat/test-company_properties.R
@@ -0,0 +1,7 @@
+context("test-company_properties")
+
+test_that("company_properties works", {
+  res <- company_properties()
+  expect_is(res, "tbl_df")
+  expect_gt(ncol(res), 3)
+})
