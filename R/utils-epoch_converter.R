0f8428cbeeae62781226a8fffc6e221146152b63
Steph Locke <steph@itsalocke.com>
2018-12-28 12:53:54 +0000

    lint

--- R/utils-epoch_converter.R
+++ R/utils-epoch_converter.R
@@ -5,13 +5,14 @@
 #'
 epoch_converter <- function(dt) {
   as_data_frame(
-    lapply(dt, function(x){
-      if(is.factor(x)|is.character(x)) {
+    lapply(dt, function(x) {
+      if (is.factor(x) | is.character(x)) {
         x
-      } else if ( suppressWarnings(min(x, na.rm =  TRUE)) > 1e+10) {
+      } else if (suppressWarnings(min(x, na.rm = TRUE)) > 1e+10) {
         as.POSIXct(x / 1000, origin = "1970-01-01")
       } else {
         x
       }
-    }))
+    })
+  )
 }
