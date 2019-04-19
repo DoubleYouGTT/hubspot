470baa4f52e431d4338c1d03e9128a7d5c8505f4
Steph Locke <steph@itsalocke.com>
2019-04-19 10:20:22 +0100

    More movement in the API

--- tests/testthat/test-deal_pipelines.R
+++ tests/testthat/test-deal_pipelines.R
@@ -3,5 +3,5 @@ context("test-deal_pipelines")
 test_that("deal_pipelines works", {
   res <- deal_pipelines()
   expect_is(res, "tbl_df")
-  expect_equal(ncol(res), 8)
+  expect_equal(ncol(res), 9)
 })
