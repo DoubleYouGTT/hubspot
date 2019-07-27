b036e2f37c807ed42a76f0cf99f4b2e73b98a3be
deneara <jgeahlen@gmail.com>
2019-07-27 11:36:36 -0400

    Updated test inline with current Hubspot version

--- tests/testthat/test-deal_stages_history.R
+++ tests/testthat/test-deal_stages_history.R
@@ -3,5 +3,5 @@ context("test-deal_stages_history")
 test_that("deal_stages_history works", {
   res <- deal_stages_history()
   expect_is(res, "tbl_df")
-  expect_equal(ncol(res), 6)
+  expect_equal(ncol(res), 5)
 })
