cd07b3ba8b9a32c627ca661e5c7788b780a627e4
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-11-18 09:51:24 +0100

    simpler

--- R/get_deal_pipelines.R
+++ R/get_deal_pipelines.R
@@ -14,17 +14,9 @@ get_deal_pipelines <- function(apikey = hubspot_key_get(),
     include_inactive,
     c("EXCLUDE_DELETED", "INCLUDE_DELETED")
   )
-  base_url <- "https://api.hubapi.com"
-  properties_url <- httr::modify_url(base_url,
-    path = "/crm-pipelines/v1/pipelines/deals/"
-  )
-  res <- httr::GET(properties_url,
-    query = list(
-      hapikey = apikey,
-      includeInactive = include_inactive
-    )
-  )
-  pipelines <- httr::content(res)
 
-  return(pipelines)
+  get_results(path = "/crm-pipelines/v1/pipelines/deals/",
+              apikey = apikey,
+              query = list(includeInactive = include_inactive))
+
 }
