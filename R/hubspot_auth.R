d8bbf4c3ff0bbf18e6d086ae3d3bf61409cbe6ea
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-11-25 12:57:44 +0100

    more docs

--- R/hubspot_auth.R
+++ R/hubspot_auth.R
@@ -1,37 +1,3 @@
-#' LD Hubspot App info
-#' @return Named list
-#' @noRd
-ld_hubspot_app <- function() {
-  list(client_secret = "2c0b347e-1e2a-4db7-a31a-aaa0b2fcad13",
-       client_id = "5975ba5c-900c-4fd7-94dd-04df023e4263",
-       app_id = "205749")
-}
-
-#' Authorize URL for OAuth
-#' @return Authorize URL (character)
-#' @noRd
-authorize_url <- function() {
-  paste0("https://app.hubspot.com/oauth/authorize?client_id=",
-         ld_hubspot_app()$client_id,
-         "&scope=contacts%20content%20forms%20tickets%20e-commerce")
-}
-
-#' Access URL for OAuth
-#' @return Access URL (character)
-#' @noRd
-access_url <- function() {
-  "https://api.hubapi.com/oauth/v1/token"
-}
-
-#' Endpoint for OAuth
-#' @return OAuth endpoint (httr special class)
-#' @noRd
-hubspot_oauth_endpoint <- function(){
-  endpoint <- httr::oauth_endpoint(authorize = authorize_url(),
-                                   access = access_url())
-
-}
-
 #' Auth info from parameters provided
 #' @return Named list
 #' @noRd
@@ -52,26 +18,3 @@ hubspot_auth <- function(token_path, apikey) {
   }
 }
 
-hubspot_oauth_app <- function() {
-
-  oauth_app <- httr::oauth_app(appname = "MyHubSpotApp",
-                               key = ld_hubspot_app()$client_id,
-                               secret = ld_hubspot_app()$client_secret)
-}
-
-
-#' Create Hubspot authorization token.
-#'
-#' @includeRmd vignettes/rmdhunks/auth.Rmd
-#'
-#' @return
-#' @family auth
-#' @export
-#'
-#' @examples
-hubspot_token_get <- function() {
-
-  token <- httr::oauth2.0_token(endpoint = hubspot_oauth_endpoint(),
-                                oauth_app = hubspot_oauth_app())
-
-}
---
+++ R/hubspot_oauth.R
@@ -0,0 +1,76 @@
+#' LD Hubspot App info
+#' @return Named list
+#' @keywords internal
+#' @export
+ld_hubspot_app <- function() {
+  list(client_secret = "2c0b347e-1e2a-4db7-a31a-aaa0b2fcad13",
+       client_id = "5975ba5c-900c-4fd7-94dd-04df023e4263",
+       app_id = "205749",
+       scope = c("contacts", "content", "forms", "tickets",
+                  "e-commerce"))
+}
+
+
+#' Make a string about scope for the authorize URL
+#' @noRd
+make_scopes_string <- function(scope){
+  scope_part <- glue::glue_collapse(scope, sep = "%20")
+  glue::glue("&scope={scope_part}")
+  "&scope=contacts%20content%20forms%20tickets%20e-commerce"
+}
+
+#' Authorize URL for OAuth
+#' @return Authorize URL (character)
+#' @noRd
+authorize_url <- function(app_info) {
+  paste0("https://app.hubspot.com/oauth/authorize?client_id=",
+         app_info$client_id,
+         make_scopes_string(app_info$scope)
+         )
+}
+
+#' Access URL for OAuth
+#' @return Access URL (character)
+#' @noRd
+access_url <- function() {
+  "https://api.hubapi.com/oauth/v1/token"
+}
+
+#' Endpoint for OAuth
+#' @return OAuth endpoint (httr special class)
+#' @noRd
+hubspot_oauth_endpoint <- function(app_info){
+  endpoint <- httr::oauth_endpoint(authorize = authorize_url(app_info),
+                                   access = access_url())
+
+}
+
+hubspot_oauth_app <- function(app_info) {
+
+  oauth_app <- httr::oauth_app(appname = "MyHubSpotApp",
+                               key = app_info$client_id,
+                               secret = app_info$client_secret)
+}
+
+
+#' Create Hubspot authorization token.
+#'
+#' @includeRmd vignettes/rmdhunks/oauth.Rmd
+#' @includeRmd vignettes/rmdhunks/auth.Rmd
+#'
+#' @param app_info A named list with client_secret, client_id, app_id and
+#' scopes.
+#'
+#' @return
+#' @family auth
+#' @export
+#'
+#' @examples
+hubspot_token_get <- function(app_info = ld_hubspot_app()) {
+
+  token <- httr::oauth2.0_token(endpoint =
+                                  hubspot_oauth_endpoint(app_info),
+                                app =
+                                  hubspot_oauth_app(app_info))
+
+}
