cd1b2c04c622259e09f75a8cfc069484a986b124
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-12-02 12:36:38 +0100

    fixes

--- tests/testthat/test-oauth.R
+++ tests/testthat/test-oauth.R
@@ -3,5 +3,5 @@ test_that("access with oauth works", {
   library("hubspot")
   contacts <- get_contacts(token_path = ".hubspot_token.rds")
   expect_is(contacts, "list")
-  expect_equal(length(contacts), 3)
+  expect_equal(length(contacts), 2)
 })
