720015002f98710bf5b451706d0a8a79c737a187
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-12-02 11:32:35 +0100

    load hubspot

--- tests/testthat/test-oauth.R
+++ tests/testthat/test-oauth.R
@@ -1,5 +1,6 @@
 test_that("access with oauth works", {
   skip_if_not(file.exists(".hubspot_token.rds"))
+  library("hubspot")
   contacts <- get_contacts(token_path = ".hubspot_token.rds")
   expect_is(contacts, "list")
   expect_equal(length(contacts), 2)
