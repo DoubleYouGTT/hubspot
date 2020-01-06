ff2be58579f1ef49b959ed820c5d87077f392f95
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-06 13:54:04 +0100

    add test

--- tests/testthat/test-get_deals.R
+++ tests/testthat/test-get_deals.R
@@ -1,6 +1,20 @@
 vcr::use_cassette("get_deals", {
   test_that("get_deals works", {
-    res <- get_deals(max_iter = 1, max_properties = 1)
+    res <- get_deals(max_iter = 1, max_properties = 1,
+                     property_history = FALSE)
     expect_is(res, "list")
   })
+
+  test_that("get_deals works with history", {
+    res <- get_deals(max_iter = 1, max_properties = 2,
+                     property_history = FALSE)
+    res2 <- get_deals(max_iter = 1, max_properties = 2,
+                     property_history = TRUE)
+    expect_is(res2, "list")
+    expect_equal(length(res$`931633510`$properties$hs_lastmodifieddate$versions),
+                 1)
+    expect_equal(length(res2$`931633510`$properties$hs_lastmodifieddate$versions),
+                 3)
+  })
 })
+
