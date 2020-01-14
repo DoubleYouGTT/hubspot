2330715b794b0a33fbb09f2a47aef143c366903a
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-14 07:05:42 +0100

    simpler

--- tests/testthat/test-oauth.R
+++ tests/testthat/test-oauth.R
@@ -1,13 +1,14 @@
 test_that("access with oauth works", {
   skip_if_not(file.exists(".hubspot_token.rds"))
-  contacts <- hubspot::hs_contacts_raw(token_path = ".hubspot_token.rds")
+  devtools::load_all(path = gsub("\\/tests\\/testthat\\/", "", getwd()))
+  contacts <- hs_contacts_raw(token_path = ".hubspot_token.rds")
   expect_is(contacts, "list")
   expect_equal(length(contacts), 2)
 })
 
 test_that("access with oauth and another app works", {
   skip_if_not(file.exists(".hubspot_otherapp.rds"))
-  contacts <- hubspot::hs_contacts_raw(token_path = ".hubspot_otherapp.rds")
+  contacts <- hs_contacts_raw(token_path = ".hubspot_otherapp.rds")
   expect_is(contacts, "list")
   expect_equal(length(contacts), 2)
 })
