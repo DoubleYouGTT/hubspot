570cad833ccd8bd765894ecd0fcea2fe7f0025cf
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-14 06:57:02 +0100

    try

--- tests/testthat/test-oauth.R
+++ tests/testthat/test-oauth.R
@@ -1,15 +1,13 @@
 test_that("access with oauth works", {
   skip_if_not(file.exists(".hubspot_token.rds"))
-  library("hubspot")
-  contacts <- hs_contacts_raw(token_path = ".hubspot_token.rds")
+  contacts <- hubspot::hs_contacts_raw(token_path = ".hubspot_token.rds")
   expect_is(contacts, "list")
   expect_equal(length(contacts), 2)
 })
 
 test_that("access with oauth and another app works", {
   skip_if_not(file.exists(".hubspot_otherapp.rds"))
-  library("hubspot")
-  contacts <- hs_contacts_raw(token_path = ".hubspot_otherapp.rds")
+  contacts <- hubspot::hs_contacts_raw(token_path = ".hubspot_otherapp.rds")
   expect_is(contacts, "list")
   expect_equal(length(contacts), 2)
 })
